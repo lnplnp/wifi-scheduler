8374b76386be617319b3a1eeee4bbf7e964f8acf
Manuel PAYET <manuelpayet@gmail.com>
2013-01-04 18:09:08 +0100

    WIFI Scheduler : Initialisation

--- git_change_email.bat
+++
@@ -1,9 +0,0 @@
-git filter-branch --commit-filter '
-        if [ "$GIT_AUTHOR_EMAIL" = "mpayet@ceritek.com" ];
-        then
-                GIT_AUTHOR_NAME="Manuel PAYET";
-                GIT_AUTHOR_EMAIL="manuelpayet@gmail.com";
-                git commit-tree "$@";
-        else
-                git commit-tree "$@";
-        fi' HEAD
